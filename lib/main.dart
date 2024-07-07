60c59c26dfc8014c742cff2fde740bdcd41a5d4d
권경민 <phillip0919@gmail.com>
2024-07-07 15:26:23 +0900

    <feat>: 박스 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,42 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('앱임'), backgroundColor: Colors.blue,),
-        body: Text("Hello World!"),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Hello World'),
+          backgroundColor: Colors.green,
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          height: 150,
+          child: Row(
+            children: [
+              Image.asset('carina.jpg', width: 150,),
+              Container(
+                width: 150,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('Carina'),
+                    Text('유지민'),
+                    Text('aespa'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text("4")
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
         )
       )
     );
