dbbb91bec43a45713934920c38cece801aabfc2e
권경민 <phillip0919@gmail.com>
2024-07-23 17:39:04 +0900

    <feat>: Flutter + Firebase로 넷플릭스 UI 클론 코딩하기 [무작정 플러터] 2강

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_toy/screen/home_screen.dart';
 import 'package:flutter_toy/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
@@ -22,7 +23,7 @@ class _MyAppState extends State<MyApp> {
               body: TabBarView(
                 physics: NeverScrollableScrollPhysics(),
                 children: [
-                  Container(child: Center(child: Text("Home"))),
+                  HomeSceen(),
                   Container(child: Center(child: Text("search"))),
                   Container(child: Center(child: Text("save"))),
                   Container(child: Center(child: Text("more")))
